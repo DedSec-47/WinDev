ec6a38e204cd0ee858091b056b147f4e4e640b29
Codename 47 <154718593+DedSec-47@users.noreply.github.com>
2025-07-28 17:08:46 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function copyCode() {
-  const code = document.querySelector("pre code").textContent;
-  navigator.clipboard.writeText(code).then(() => {
-    alert("Code copied to clipboard!");
-  }).catch(err => {
-    alert("Failed to copy code: " + err);
-  });
-}
